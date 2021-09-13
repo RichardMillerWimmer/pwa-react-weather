f34bd7abf3f3ec0836cfea8ee5f5137b710e7d7d
Richard Miller <richardmmill@gmail.com>
2021-09-13 16:43:03 -0400

    api working

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-import dotenv from 'dotenv';
-dotenv.config();
-const { API_KEY } = process.env
 
-console.log(API_KEY)
 
 const url = `https://api.openweathermap.org/data/2.5/weather`
 
@@ -12,8 +8,9 @@ export const fetchWeather = async (query) => {
     const { data } = await axios.get(url, {
         params: {
             q: query,
-            appid: `${API_KEY}`
+            appid: `${process.env.REACT_APP_API_KEY}`
         }
     });
+    // console.log(data)
     return data;
 };
